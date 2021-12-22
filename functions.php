4161308e9b708da6b6ba4933e7dddf785b732be4
Tinashe Jamanda <ZimboNash@.github.com>
2021-12-22 13:37:27 +0200

    connected cleardb heroku

--- functions.php
+++ functions.php
@@ -1,11 +1,24 @@
 <?php // functions.php
-$dbhost = 'localhost';
-$dbname = 'iutopia';
-$dbuser = 'root';
-$dbpass = '';
-$appname = "THE iDEA OF UTOPIA";
-$dbserver = mysqli_connect($dbhost, $dbuser, $dbpass) or die(mysqli_error($dbserver));
- mysqli_select_db($dbserver, $dbname) or die();
+// $dbhost = 'localhost';
+// $dbname = 'iutopia';
+// $dbuser = 'root';
+// $dbpass = '';
+// $appname = "THE iDEA OF UTOPIA";
+// $dbserver = mysqli_connect($dbhost, $dbuser, $dbpass) or die(mysqli_error($dbserver));
+//  mysqli_select_db($dbserver, $dbname) or die();
+
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$dbserver = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
+mysqli_select_db($cleardb_server, $cleardb_db) or die();
 
 function querymysql($dbserver, $query)
 {
